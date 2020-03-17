dd42de20f14c4f5560b5cb8ba69e680c3c636591
Daniel Pozmanter <danpozmanter+github@gmail.com>
2020-03-17 00:26:24 -0400

    Cleanup

--- be_fsharp_dotnet_core/TodoList/Models/TodoTask.fs
+++
@@ -1,8 +0,0 @@
-namespace Todolist.Models
-
-// Naming TodoTask to avoid name conflict with threading Task
-type TodoTask () =
-    member val Id : int = 0 with get, set
-    member val Text : string = "" with get, set
-    member val Priority : int = 0 with get, set
-    
