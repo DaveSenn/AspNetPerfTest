7a503ab004f9a22e7c557b17cf0735e3776fcac9
Daniel Pozmanter <danpozmanter+github@gmail.com>
2020-04-21 17:29:34 -0400

    updating initialize db script

--- be_python_sanic/initialize_db.py
+++ be_python_sanic/initialize_db.py
@@ -1,5 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from models import metadata
 
-engine = create_engine('postgresql://tdl_user:secretz@localhost/todolist')
+engine = create_engine('postgresql://{}:{}@{}/todolist'.format(
+    os.environ('DEV_PG_USER'),
+    os.environ('DEV_PG_PASSWORD'),
+    os.environ('DEV_PG_HOST')
+))
 metadata.create_all(engine)
