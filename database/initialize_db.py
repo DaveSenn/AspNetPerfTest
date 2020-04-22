3a5747b8b8d87963baad70134335ba4cb4402dde
Daniel Pozmanter <danpozmanter+github@gmail.com>
2020-04-22 15:34:21 -0400

    style fix

--- database/initialize_db.py
+++ database/initialize_db.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 import sys
-sys.path.append("../be_python_sanic/")
+sys.path.append('../be_python_sanic/')
 from models import metadata
 
 engine = create_engine('postgresql://{}:{}@{}/todolist'.format(
